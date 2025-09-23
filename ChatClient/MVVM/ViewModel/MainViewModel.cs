b2ff76c5bd62f5200e8b505060a8d459c64831e2
Jeryan Eka Yuda <jejey09@gt.student.pens.ac.id>
2025-09-23 20:26:06 +0700

    Add project files.

---
+++ ChatClient/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,105 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.MVVM.Model;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+        public RelayCommand ConnectToServerCommand { get; set; }
+        public RelayCommand SendMessageCommand { get; set; }
+        public RelayCommand DisconnectFromServerCommand { get; set; }
+
+
+        private Server _server;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectEvent += RemoveUser;
+
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o=> !string.IsNullOrEmpty(Username));
+
+            SendMessageCommand = new RelayCommand(o =>
+            {
+                _server.SendMessageToServer(Message);
+                Message = string.Empty;  
+                OnPropertyChanged(nameof(Message)); 
+            },
+            o => !string.IsNullOrEmpty(Message)); DisconnectFromServerCommand = new RelayCommand(o => _server.DisconnectFromServer(), o => _server.IsConnected);
+
+
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage()
+            };
+
+            if(!Users.Any(x => x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+        }
+    }
+}
---
+++ ChatClient/Net/Server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using ChatClient.Net.IO;
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+        public PacketReader PacketReader;
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action userDisconnectEvent;
+        public bool IsConnected => _client?.Connected ?? false;
+
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer(string username)
+        {
+            if(!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                PacketReader = new PacketReader(_client.GetStream());
+                if(!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteMessage(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch(opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectEvent?.Invoke();
+                            break;
+                        default:
+                            Console.WriteLine("Tes");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+
+        public void DisconnectFromServer()
+        {
+            if (_client != null && _client.Connected)
+            {
+                _client.Close();
+                Console.WriteLine("Disconnected from server.");
+            }
+        }
+    }
+}
